af5310354c269ae3132626d744d26ce6c982b249
Skander Moalla <skander.moalla@epfl.ch>
2024-09-03 15:56:22 +0200

    relaxed dependencies

--- setup.py
+++ setup.py
@@ -87,8 +87,8 @@ EXTRAS = {
     "liger": ["liger-kernel>=0.2.1"],
     "diffusers": ["diffusers>=0.18.0"],
     "deepspeed": ["deepspeed>=0.14.4"],
-    "benchmark": ["wandb", "ghapi", "openrlbenchmark==0.2.1a5", "requests", "deepspeed"],
-    "quantization": ["bitsandbytes<=0.41.1"],
+    "benchmark": ["wandb", "ghapi", "requests", "deepspeed"],
+    "quantization": ["bitsandbytes"],
     "llm_judge": ["openai>=1.23.2", "huggingface_hub>=0.22.2", "llm-blender>=0.0.2"],
 }
 EXTRAS["dev"] = []
